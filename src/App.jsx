368d99a11122986751a1f39ea4c8394b44f3f90f
PaballoRafutho <paballo.ptr@gmail.com>
2022-10-31 21:26:10 +0200

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './components/Header/Header'
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
-
 const App = () => {
   return (
     <>
